84a9cbac38f53d9fe7c25a9ed052d14edfe3463b
Eternally-carry <2483522462@qq.com>
2024-07-22 00:46:17 +0800

    对maim部分的主要处理函数进行定义

--- progam.c
+++ progam.c
@@ -1,5 +1,16 @@
 # include<stdio.h>
 int main(void)
 {
-    int git;
+    double input_Celsius(double,int); //输入摄氏温度的函数
+    double input_Kelvin_temperature(double,int); //输入开氏温度的函数
+    double input_Fahrenheit(double,int); //输入华氏温度的函数
+    const double Celsius_And_Kelvin_temperature = 273.16;//摄氏温度和开氏温度
+    const double Celsius_And_Fahrenheit = 32.0; //华氏温度和摄氏温度
+    int input_unit; //用于记录输入单位
+    int output_unit; //用于记录输出单位
+    /*
+        开氏温度单位选择值为：1
+        摄氏温度单位选择值为：2
+        华氏温度单位选择值为：3
+    */
 }
